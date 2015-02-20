eeff34f00efde687242a3ca3ccae317f3e59476f
Yunus YEN <yunusyenigor@hotmail.com>
2015-02-19 17:03:47 -0800

    Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not necessarily a binary search tree.

--- commonAncestor.js
+++ commonAncestor.js
@@ -1,15 +1,14 @@
 var BST = require('./BST');
-var b1 = new BST();
-var b2 = new BST();
-b1.insert(3);
-b1.insert(1);
-b1.insert(5);
-b1.insert(9);
-b1.insert(4);
-b1.insert(8);
-b1.insert(21);
-b1.insert(7);
-b1.insert(30);
+var b = new BST();
+b.insert(3);
+b.insert(1);
+b.insert(5);
+b.insert(9);
+b.insert(4);
+b.insert(8);
+b.insert(21);
+b.insert(7);
+b.insert(30);
 
 function commonAncestor(node1, node2){
 	var n1 = node1;
@@ -25,7 +24,7 @@ function commonAncestor(node1, node2){
 	return false;
 }
 
-var node1 = b1.search(b1.root, 4);
-var node2 = b1.search(b1.root, 7)
+var node1 = b.search(b.root, 4);
+var node2 = b.search(b.root, 7)
 //console.log(node2);
 console.log(commonAncestor(node1, node2));
