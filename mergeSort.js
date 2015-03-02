f23f766a02c0c73a368acc25ed16cf6c744be5db
Yunus YEN <yunusyenigor@hotmail.com>
2015-03-02 15:49:13 -0800

    Optimized the code

--- mergeSort.js
+++ mergeSort.js
@@ -10,37 +10,18 @@ function merge(array, p, q, r){
 	var lowHalf = [];
 	var highHalf = [];
 	var k = p;
-	var i;
-	var j;
-	for(i=0; k<=q; i++, k++){
-		lowHalf[i] = array[k];
-	}
-	for(j=0; k<=r; j++, k++){
-		highHalf[j] = array[k];
-	}
+	var i = 0;
+	var j = 0;
+	while(k <= q)	lowHalf[i++] = array[k++];
+	while(k <= r)	highHalf[j++] = array[k++];
 	i = 0;
 	j = 0;
 	k = p;
-	while(i < lowHalf.length && j < highHalf.length){
-		if(lowHalf[i] < highHalf[j]){
-			array[k] = lowHalf[i];
-			i++;
-		} else {
-			array[k] = highHalf[j]
-			j++;
-		}
-		k++;
-	}
-	while(i < lowHalf.length){
-		array[k] = lowHalf[i];
-		i++;
-		k++;
-	}
-	while(j < highHalf.length){
-		array[k] = highHalf[j];
-		j++;
-		k++;
-	}
+	while(i < lowHalf.length && j < highHalf.length)
+		if(lowHalf[i] < highHalf[j])	array[k++] = lowHalf[i++];
+		else	array[k++] = highHalf[j++]
+	while(i < lowHalf.length)	array[k++] = lowHalf[i++];
+	while(j < highHalf.length)	array[k++] = highHalf[j++];
 }
 
 var arr = [77, -12, 21, 0, 1, 13, -2, 4, 19, 99, 500];
