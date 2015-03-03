d46559e023eeda14a03d63cb8b9c63af24bb1c7a
Yunus YEN <yunusyenigor@hotmail.com>
2015-03-02 18:47:00 -0800

    Searching inside Matrix in Java

---
+++ MatrixSearch.java
@@ -0,0 +1,22 @@
+class MatrixSearch {
+	
+	public static boolean search(int[][] matrix, int target){
+		int row = 0;
+		int col = matrix[0].length-1;
+		while(row < matrix.length && col >= 0){
+			if(matrix[row][col] == target){
+				return true;
+			} else if(matrix[row][col] > target){
+				col--;
+			} else {
+				row++;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int[][] arr = {{15, 20, 40, 85}, {20, 35, 80, 95}, {30, 55, 95, 105}, {40, 80, 100, 120}};
+		System.out.print(search(arr, 80));
+	}
+}
