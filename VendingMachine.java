3636deca0799c2835c7fc97320b282671fe35ab4
Yunus YEN <yunusyenigor@hotmail.com>
2015-03-03 15:47:11 -0800

    Vending Machine written in Java

---
+++ VendingMachine.java
@@ -0,0 +1,22 @@
+class VendingMachine {
+	
+	class CardReader {
+		
+	}
+	
+	class Change {
+		
+	}
+	
+	class Product {
+		
+	}
+	
+	class Outbox {
+		boolean dropped = false;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
