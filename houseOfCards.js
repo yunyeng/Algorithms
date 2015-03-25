c4cd0037daa02a196abea4c0dab77227a869f794
Yunus YEN <yunusyenigor@hotmail.com>
2015-03-25 15:14:18 -0700

    How many cards needed for x floors of HouseOfCards

---
+++ houseOfCards.js
@@ -0,0 +1,20 @@
+function houseOfCards(floors){
+	if(floors <= 0){
+		return 0;
+	}
+	var cards = 7;
+	var total = cards;
+	if(floors == 1){
+		return cards;
+	}
+	cards++;
+	total += cards;
+	for(var i=2; i<floors; i++){
+		cards += 3;
+		total += cards;	
+	}		
+	return total;
+}
+
+console.log(houseOfCards(1)); // 7
+console.log(houseOfCards(6)); // 26
